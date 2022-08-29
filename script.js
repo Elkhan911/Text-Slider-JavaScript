b40128a596f074575872117238745262a651200d
Elkhan911 <955pro@mail.ru>
2022-08-29 18:57:16 +0500

    added quotes and delete redundant code

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ const rightBtn = document.querySelector("#_rightBtn");
 
 // массив текста
 let arr = [
-  "text 0",
-  "text 1",
-  "text 2",
-  "text 3",
-  "text 4",
-  "text 5",
-  "text 6",
-  "text 7",
-  "text 8",
-  "text 9",
-  "text 10",
+  "Все дело в мгновении. Оно определяет жизнь",
+  "Не понимаю, почему люди боятся новых идей. Я боюсь старых",
+  "Счастье охотнее заходит в тот дом, где всегда царит хорошее настроение",
+  "Если это не весело, значит вы делаете это неправильно",
+  "Сторонитесь людей, которые пытаются уменьшить ваши амбиции",
+  "Если вы сами цените себя невысоко, мир не предложит вам ни на грош больше",
+  "Не имея возможности жить вечно, мы имеем возможность жить ярко",
+  "Вечность влюблена в творения времени",
+  "Жизнь — это поезд, займи свое место",
+  "Где много любви, там много ошибок. Где нет любви, там все ошибка",
+  "Почувствуй себя сегодня тем, кем хочешь стать завтра",
+  "С самого низа все пути ведут наверх",
+  "Этот миг и есть твоя жизнь",
+  "Доверие — это важно. А, точнее, — это всё",
+  "Работай над собой. Никто не влюбится с первого взгляда в твой богатый внутренний мир",
+  "Бог никогда не покидал тебя",
+  "Сломанные мелки по-прежнему цветные",
+  "Мечтай о большем. Делай больше",
 ];
 
 // счетчик для таймера
@@ -32,12 +39,11 @@ function timerStart() {
   timerId = setInterval(function () {
     timerCounter++;
     text.textContent = arr[timerCounter];
-    console.log(timerCounter);
     if (arr.length <= timerCounter) {
       timerCounter = 0;
       text.textContent = arr[timerCounter];
     }
-  }, 1000);
+  }, 5000);
 
   startBtn.removeEventListener("click", timerStart);
 }
@@ -48,23 +54,26 @@ function timerPause() {
   startBtn.addEventListener("click", timerStart);
 }
 
-startBtn.addEventListener("click", timerStart);
-stopBtn.addEventListener("click", timerPause);
-
-leftBtn.addEventListener("click", function () {
+// функция пролистнуть слайдер вперед
+function goForward() {
   text.textContent = arr[timerCounter];
   if (timerCounter <= 1) {
     timerCounter = 1;
   }
   timerCounter--;
-  console.log(timerCounter);
-});
+}
 
-rightBtn.addEventListener("click", function () {
+// функция пролистнуть слайдер назад
+function goBack() {
   text.textContent = arr[timerCounter];
-  if (timerCounter == 10) {
+  if (timerCounter == arr.length - 1) {
     timerCounter = 9;
   }
   timerCounter++;
-  console.log(timerCounter);
-});
+}
+
+startBtn.addEventListener("click", timerStart);
+stopBtn.addEventListener("click", timerPause);
+
+leftBtn.addEventListener("click", goForward);
+rightBtn.addEventListener("click", goBack);
